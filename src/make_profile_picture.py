5b593d17c99df678b76b5a0f93781da1a6fa15bf
Christopher Mansueti <mansuet2@msu.edu>
2018-07-11 21:38:43 -0400

    added command line argument handling.
    usage: python make_profile_picture.py [--size INT] image_path
    example: python make_profile_picture.py --size 526 test.png

--- src/make_profile_picture.py
+++ src/make_profile_picture.py
@@ -1,3 +1,4 @@
+import sys
 from PIL import Image, ImageDraw, ImageOps
 
 def open_image(image_path):
@@ -26,11 +27,26 @@ def create_thumbnail(image, thumbnail_size=None):
         fill=255,
     )
 
-    resized_image = ImageOps.fit(image, circle_overlay.size, centering=(0.5, 0.5))
+    resized_image = ImageOps.fit(image, circle_overlay.size, Image.ANTIALIAS, centering=(0.5, 0.5))
     resized_image.putalpha(circle_overlay)
     return resized_image
     
+def main():
+    try:
+        assert(len(sys.argv) > 1)
 
-# im = open_image("test.jpg")
-# create_thumbnail(im,256).show()
-# create_thumbnail(im).show()
+    except AssertionError:
+        print("Error. Did not supply image path")
+        return(-1)
+    print(str(sys.argv))
+    thumbnail_size = 256
+    if '--size' in sys.argv:
+        size_flag_index = sys.argv.index('--size')
+        thumbnail_size = int(sys.argv[size_flag_index+1])
+    source_image = open_image(sys.argv[-1])
+    create_thumbnail(source_image, thumbnail_size).show()
+main()
+#im = open_image("test.jpg")
+#im.show()
+#create_thumbnail(im,256).show()
+#create_thumbnail(im).show()
